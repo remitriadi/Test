3ad263cf0728d156df3775f36a21f0e40050b32e
REMI TRIADI <remitriadi@gmail.com>
2021-08-19 05:24:44 +0700

    latest update

--- dist/work.8f2842cd.js
+++ dist/work.8f2842cd.js
@@ -396,7 +396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64239" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50685" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
